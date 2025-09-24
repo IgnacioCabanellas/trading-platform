4c962dcd465e22cfc651d57f3e250aa710cf416e
LucasCordoba <52933262+lcordoba844@users.noreply.github.com>
2025-09-24 16:33:30 -0300

    Added asset service

---
+++ backend/src/application/asset/asset.dto.ts
@@ -0,0 +1,74 @@
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+} from "class-validator";
+
+export class GetAssetRequest {
+  @IsString()
+  @Length(1, 100)
+  @IsNotEmpty()
+  name!: string;
+
+  @IsString()
+  @Length(1, 10)
+  symbol!: string;
+
+  @IsString()
+  @Length(1, 300)
+  description!: string;
+
+  @IsBoolean()
+  enabled!: boolean;
+}
+
+export class CreateAssetRequest {
+  @IsString()
+  @Length(1, 10)
+  @IsNotEmpty()
+  symbol!: string;
+
+  @IsString()
+  @Length(1, 100)
+  @IsNotEmpty()
+  name!: string;
+
+  @IsString()
+  @Length(1, 300)
+  description?: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  enabled!: boolean;
+}
+
+export class UpdateAssetRequest {
+  @IsOptional()
+  @IsString()
+  @Length(1, 10)
+  symbol?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(1, 100)
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(1, 300)
+  description?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  enabled?: boolean;
+}
+
+export interface AssetResponse {
+  id: string;
+  symbol: string;
+  name: string;
+  description: string;
+  enabled: boolean;
+}
