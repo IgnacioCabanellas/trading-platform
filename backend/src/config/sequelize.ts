d2d8b77084618aac74c5e0b2df7d7ce377863ff7
Francisco Nicola <49844500+francisconicola@users.noreply.github.com>
2025-10-16 21:20:31 -0300

    Add balance and movements

--- backend/src/config/sequelize.ts
+++ backend/src/config/sequelize.ts
@@ -2,11 +2,20 @@ import { ModelCtor, Sequelize } from 'sequelize-typescript';
 
 import { environment } from '@/config/environment';
 import { Asset } from '@/models/asset.model';
+import { Balance } from '@/models/balance.model';
 import { Limit } from '@/models/limit.model';
+import { Movement } from '@/models/movement.model';
 import { TradingPair } from '@/models/trading-pair.model';
 import { User } from '@/models/user.model';
 
-const models: ModelCtor[] = [User, Limit, Asset, TradingPair];
+const models: ModelCtor[] = [
+  User,
+  Limit,
+  Asset,
+  TradingPair,
+  Balance,
+  Movement,
+];
 
 export const sequelize = new Sequelize(environment.databaseUrl, {
   dialect: 'postgres',
